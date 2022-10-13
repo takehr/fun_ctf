0b221c218655793afac6537f450c823554a0569a
takehr <takehiro.cit@gmail.com>
2022-10-14 03:04:57 +0900

    add editorial

--- fun_ctf/src/App.tsx
+++ fun_ctf/src/App.tsx
@@ -2,27 +2,58 @@ import React, { useEffect, useRef, useState } from 'react';
 import { JsxElement } from 'typescript';
 import './App.css';
 
-function Problem(props:{title:string, problemSentences:any, ans:string}){
+function Problem(props:{title:string, problemSentences:any, ans:string, editorial:any}){
   const [clicked,setClicked] = useState<number>(0);
+  const [checkEditorial,setCheckEditorial] = useState<number>(0);
   const [flag, setFlag] = useState<string>("")
   return (
       <div className='list-element' onClick={()=>setClicked(clicked===0?1:-clicked)}>
         {props.title}
         {
           clicked === 1 ?
-            <div className='problem-sentences-inflate'>
-              <br></br>{props.problemSentences}
-              <input type="text" value={flag} onClick={(e)=>{e.stopPropagation();}} onChange= {e => setFlag(e.target.value)} placeholder="flag"></input>
-              <br></br>
-              <button onClick={(e)=>{e.preventDefault();e.stopPropagation();if(flag===props.ans)alert("Correct!!");else alert("flase");}}>submit</button>
-            </div>
+            checkEditorial?
+              <div className='problem-sentences-inflate'>
+                {props.editorial}
+              </div>
+            :
+              <div className='problem-sentences-inflate'>
+                <br></br>{props.problemSentences}
+                <input type="text" value={flag} onClick={(e)=>{e.stopPropagation();}} onChange= {e => setFlag(e.target.value)} placeholder="flag"></input>
+                <br></br>
+                <button onClick={(e)=>{
+                  e.preventDefault();
+                  e.stopPropagation();
+                  if(flag===props.ans){
+                    alert("Correct!!");
+                  }else alert("flase");
+                  }}>submit</button>
+                  <br></br>
+                <button style={{textAlign:"right",width:"800px"}} onClick={(e)=>{e.stopPropagation(); setCheckEditorial(checkEditorial===0?1:-checkEditorial)}}>
+                  解説をみる
+                </button>
+              </div>
           :clicked===-1?
-            <div className='problem-sentences-deflate'>
-              <br></br>{props.problemSentences}
-              <input type="text" value={flag} onClick={(e)=>{e.stopPropagation();}} onChange= {e => setFlag(e.target.value)}></input>
-              <br></br>
-              <button onClick={(e)=>{e.preventDefault();e.stopPropagation();if(flag===props.ans)alert("Correct!!");else alert("flase");}}>submit</button>
-            </div>
+            checkEditorial?
+              <div className='problem-sentences-deflate'>
+                {props.editorial}
+              </div>
+            :
+              <div className='problem-sentences-deflate'>
+                <br></br>{props.problemSentences}
+                <input type="text" value={flag} onClick={(e)=>{e.stopPropagation();}} onChange= {e => setFlag(e.target.value)} placeholder="flag"></input>
+                <br></br>
+                <button onClick={(e)=>{
+                  e.preventDefault();
+                  e.stopPropagation();
+                  if(flag===props.ans){
+                    alert("Correct!!");
+                  }else alert("flase");
+                  }}>submit</button>
+                  <br></br>
+                <button style={{textAlign:"right"}}>
+                  解説をみる
+                </button>
+              </div>
           :undefined
           }
 {/*        <hr></hr>
@@ -36,11 +67,131 @@ function App() {
   var q:any = useRef(null)
 
   useEffect( ()=>{
-//    const head = document.getElementsByTagName('head')[0] as HTMLElement;
 //    const scriptUrl = document.createElement('script');
-//    scriptUrl.type = 'text/javascript';
-//    scriptUrl.src = 'http://bodytag.org/bt_melter/bt_melter.js';
-//    head.appendChild(scriptUrl);
+//    scriptUrl.innerHTML =
+//    `
+//    var bt_root = document.getElementsByTagName('body').item(0);
+//    bt_root.style.visibility = 'hidden';
+//    var bt_nodes = new Array();
+//    var bt_text = new Array();
+//    var bt_tumble = new Array();
+//    var bt_maxsize = 0;
+//    var bt_count = 0;
+//    
+//    function Is(){ 
+//      this.ver=navigator.appVersion;
+//      this.agent=navigator.userAgent;
+//      this.dom=document.getElementById?1:0;
+//      this.opera5=this.agent.indexOf("Opera 5")>-1;
+//      this.ie5=(this.ver.indexOf("MSIE 5")>-1 && this.dom && !this.opera5)?1:0; 
+//      this.ie6=(this.ver.indexOf("MSIE 6")>-1 && this.dom && !this.opera5)?1:0;
+//      this.ie4=(document.all && !this.dom && !this.opera5)?1:0;
+//      this.ie=this.ie4||this.ie5||this.ie6;
+//      this.mac=this.agent.indexOf("Mac")>-1;
+//      this.ns6=(this.dom && parseInt(this.ver) >= 5) ?1:0; 
+//      this.ns4=(document.layers && !this.dom)?1:0;
+//      this.bw=(this.ie6||this.ie5||this.ie4||this.ns4||this.ns6||this.opera5);
+//      return this;
+//      }
+//    
+//    var is = new Is();
+//    
+//    function bt_spin() {
+//      if(is.dom && !(is.mac && is.ie)) bt_godeep(bt_root);
+//      bt_root.style.visibility = 'visible';
+//      if(is.dom && !(is.mac && is.ie)) bt_sponge();
+//      }
+//    
+//    function bt_finished() {
+//      bt_nodes = null;
+//      bt_text = null;
+//      bt_tumble = null;
+//      }
+//    
+//    var bt_chartype = Math.floor(Math.random()*2);
+//    
+//    function digit() {
+//      if(bt_chartype == 0)
+//        return Math.floor(Math.random()*2);
+//      else if(bt_chartype == 1)
+//        return '_';
+//      else
+//        return '_';
+//      }
+//    
+//    var reg = new RegExp ("\\r|\\n", "g");
+//    function bt_haschars(s) {
+//      s = s.replace(reg,'');
+//      return s.length;
+//      }
+//      
+//    function bt_godeep(o) {
+//      for (var i = 0; i < o.childNodes.length; i++) {
+//        if(o.childNodes[i].childNodes) {
+//          bt_godeep(o.childNodes[i]);
+//          }
+//        if(o.childNodes[i].nodeName == '#text' && bt_haschars(o.childNodes[i].nodeValue)) {
+//          var p = bt_nodes.length;
+//          bt_nodes[p] = o.childNodes[i];
+//          bt_text[p] = o.childNodes[i].nodeValue;
+//          bt_tumble[p] = new Array();
+//          for(var u = 0; u < o.childNodes[i].nodeValue.length; u++) {
+//            bt_tumble[p][u] = u;
+//            }
+//          if(o.childNodes[i].nodeValue.length > bt_maxsize) {
+//            bt_maxsize = o.childNodes[i].nodeValue.length;
+//            }
+//          bt_tumble[p].sort(randomSort);
+//          o.childNodes[i].nodeValue = '';
+//          }
+//        }
+//      }
+//    
+//    function randomSort(w1,w2) {
+//      return Math.floor(Math.random()*3)-1;
+//      }
+//    
+//    function bt_sponge() {
+//      for (var i = 0; i < bt_nodes.length; i++) {
+//        if(bt_count < bt_tumble[i].length) {
+//          bt_nodes[i].nodeValue += (bt_text[i].charAt(bt_count) == ' ') ? ' ':digit(1);
+//          }
+//        }
+//       bt_count++;
+//       if(bt_count < bt_maxsize) {
+//        setTimeout('bt_sponge()',20);
+//        }
+//       else {
+//         bt_count = 0;
+//        setTimeout('bt_unsponge()',350);
+//        }
+//      }
+//    
+//    function bt_repchar(str, ch, pos) {
+//      var out = '';
+//      for(var i = 0; i < str.length; i++) {
+//        if(i == pos) out += ch;
+//        else out += str.charAt(i);
+//        }
+//      return out;
+//      }
+//    
+//    function bt_unsponge() {
+//      for (var i = 0; i < bt_nodes.length; i++) {
+//        if(bt_count <= bt_tumble[i].length) {
+//          bt_nodes[i].nodeValue = bt_repchar(bt_nodes[i].nodeValue, bt_text[i].charAt(bt_tumble[i][bt_count]), bt_tumble[i][bt_count]);
+//          }
+//        }
+//      bt_count++;
+//      if(bt_count < 10) setTimeout('bt_unsponge()',30);
+//      else if(bt_count < bt_maxsize) setTimeout('bt_unsponge()',5);
+//      else bt_finished();
+//      }
+//      
+//    window.onload = bt_spin;
+//    `;
+//
+//    document.head.appendChild(scriptUrl);
 
     var width = q.current.width = window.innerWidth;
     var height = q.current.height = window.innerHeight*3;
@@ -60,11 +211,8 @@ function App() {
         letters[index] = (parseFloat(y_pos) > 758 + Math.random() * 1e4) ? 0 : y_pos + 40;
       });
 
-      //左から20上から40の位置に、幅50高さ100の四角形を描く
-      
-      q.current.getContext('2d').strokeStyle = 'rgb(00,00,255)'; //枠線の色は青
-      q.current.getContext('2d').fillStyle = 'rgba(0,00,00,0.6)'; //塗りつぶしの色は赤
-//      q.current.getContext('2d').fillStyle = 'rgba(255,255,255,0.6)'; //塗りつぶしの色は赤
+      q.current.getContext('2d').strokeStyle = 'rgb(00,00,255)';
+      q.current.getContext('2d').fillStyle = 'rgba(0,00,00,0.6)';
       q.current.getContext('2d').fillRect(width/2-430,0,860,4000);
         
       //色を指定する
@@ -94,11 +242,48 @@ function App() {
             <a onClick = {(e) => e.stopPropagation()} href="https://gist.github.com/takehr/532a652a685342c0216791467eb17a71/raw/cf81c773d159fc2efa6f26565a2f59667f4343c2/capture.cap" download>
             capture&#46;cap
             </a>
-        </p>
-    ,ans:"hacking{sensei}"},
-    {title:"美しい空",problemSentences:<p>トマトはおいしい、されど空はどこまでも高く広がるようだ。まるで、でかいクッキーを取り残していくように。</p>,ans:"0023"},
-    {title:"でかいクッキー",problemSentences:<p>トマトはおいしい、されど空はどこまでも高く広がるようだ。まるで、でかいクッキーを取り残していくように。</p>,ans:"0023"},
-    {title:"扇風機",problemSentences:<p>トマトはおいしい、<br></br>されど空はどこまでも高く広がるようだ。<br></br>まるで、でかいクッキーを取り残していくように。</p>,ans:"0023"}
+        </p>,
+        ans:"hacking{sensei}",
+        editorial:
+        <div>
+          <br></br>
+          <br></br>
+          <h1>解説</h1>
+          <h3>HTTPを利用しているサイトは危険だとなぜ言われているのでしょうか？</h3>
+          <p>
+          実はHTTPのサイトを利用することが必ずしも危険というわけではありません。
+          <br></br>
+          例えば、この問題の状況では、free-wifiを利用していなければ情報の漏洩は起こらない可能性が高いでしょう。
+          <br></br>
+          また、信頼のできないサイトの場合、HTTPSを利用していても情報は漏洩する可能性があります。
+          <br></br>
+          つまり、信頼のできるサイト・通信経路を利用している場合は、HTTPの利用に危険はありません。
+          <br></br>
+          ではなぜ危険と言われるのでしょう？
+          <br></br>
+          問題は、通信経路に悪意のある第三者が介入できる状況に発生します。
+          <br></br>
+          この状況は、多くの人にとって身近な通信経路であるfree-wifiを通した通信も当てはまります。
+          <br></br>
+          free-wifiではサーバーに送信するデータをfree-wifiのパスワードを用いて暗号化されます。
+          <br></br>
+          そのため、free-wifiのパスワードを知っている人なら誰でも情報を復号化できます。
+          <br></br>
+          そのため、free-wifiを利用する多くの人にとってHTTPの利用は危険だと考えられるのです。
+          </p>
+          <br></br>
+          <br></br>
+          <h2>Coffee break</h2>
+          <h3>Wire Shark</h3>
+          <p>Wire Sharkとはネットワークのパケット解析用ソフトウェアです。
+          <br></br>
+          インストール方法、基本的な使い方、見方、読み込み法、
+          </p>
+        </div>
+  },
+    {title:"美しい空",problemSentences:<p>トマトはおいしい、されど空はどこまでも高く広がるようだ。まるで、でかいクッキーを取り残していくように。</p>,ans:"0023",editorial:""},
+    {title:"でかいクッキー",problemSentences:<p>トマトはおいしい、されど空はどこまでも高く広がるようだ。まるで、でかいクッキーを取り残していくように。</p>,ans:"0023",editorial:""},
+    {title:"扇風機",problemSentences:<p>トマトはおいしい、<br></br>されど空はどこまでも高く広がるようだ。<br></br>まるで、でかいクッキーを取り残していくように。</p>,ans:"0023",editorial:""}
   ]
 
   return (
@@ -107,7 +292,7 @@ function App() {
       <div className='Head'>fun_ctf</div>
       {
       problemList.map( (li) =>
-        <Problem title= {li.title} problemSentences={li.problemSentences} ans={li.ans}></Problem>)
+        <Problem title= {li.title} problemSentences={li.problemSentences} ans={li.ans} editorial={li.editorial}></Problem>)
       }
     </div>
   );
